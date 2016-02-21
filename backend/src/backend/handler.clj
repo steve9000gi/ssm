129e37340a8738beafc73c636500faa88cfde751
Jeff Terrell <jeff@altometrics.com>
2016-02-20 19:13:36 -0500

    Recognize and handle demographic fields at backend

--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -36,8 +36,8 @@
                  (assoc :async-channel "redacted")
                  (update-in [:body] slurp))))
 
-  (POST "/register" [email password]
-    (user/create email password))
+  (POST "/register" [email password name state affiliations reason]
+    (user/create email password name state affiliations reason))
   (GET "/testauth" {:keys [current-user-id]}
     (if current-user-id
       (resp/ok {:message "authenticated"})
--- backend/src/backend/user.clj
+++ backend/src/backend/user.clj
@@ -5,6 +5,7 @@
     [reloaded.repl :refer [system]]
     [ring.util.http-response :as resp]
     [backend.postgres :refer [insert! query]]
+    [clojure.string :as str]
     )
   (:import org.postgresql.util.PSQLException))
 
@@ -71,6 +72,26 @@
          {:auth_token {:value "", :max-age a-long-time}
           :user_id    {:value "", :max-age a-long-time}}))
 
+(defn- make-affil-map
+  "Convert a string like
+  \"affil_self_advocate,affil_local_org_staff\"
+  into a map like
+  {:affil_self_advocate true, :affil_local_org_staff true}
+  with whitelisted keys.
+  Should be robust to all manner of input."
+  [affiliations]
+  (if-not (string? affiliations)
+    {}
+    (let [parts (str/split affiliations #",")
+          whitelist #{"affil_self_advocate"
+                      "affil_family_member"
+                      "affil_health_provider"
+                      "affil_education_provider"
+                      "affil_smcha_staff"
+                      "affil_local_org_staff"}
+          whitelisted (keep whitelist parts)]
+      (into {} (map vector whitelisted (repeat true))))))
+
 (defn is-admin?
   [user-id]
   {:pre [(integer? user-id)
@@ -91,31 +112,37 @@
       (= (str (:auth_token user)) given-token))))
 
 (defn create
-  [email password]
-  (prn 'user/create email)
+  [email password name state affiliations reason]
+  (prn 'user/create email name state affiliations reason)
   (if-not (valid-email? email)
     (resp/bad-request {:message "invalid email"})
     (if-not (valid-password? password)
       (resp/bad-request {:message "invalid password"})
-      (try
-        (let [new-user (first
-                         (insert!
-                           (:db system)
-                           "ssm.users"
-                           {:email email
-                            :password (creds/hash-bcrypt
-                                       password
-                                       :work-factor password-work-factor)}))]
-          (if new-user
-            (resp/ok (select-keys new-user [:id :email]))
-            (resp/bad-request {:message "unknown error"
-                               :data new-user})))
-        (catch PSQLException e
-          (check-conflict e))
-        (catch Exception e
-          (println "Exception" e)
-          (.printStackTrace e)
-          (resp/internal-server-error {:message (.getMessage e)}))))))
+      (if (some empty? [name state reason])
+        (resp/bad-request {:message "name/state/reason are required"})
+
+        (try
+          (let [affil-map (make-affil-map affiliations)
+                user-map  (assoc affil-map
+                                 :email email
+                                 :password (creds/hash-bcrypt
+                                            password
+                                            :work-factor password-work-factor)
+                                 :name name
+                                 :state state
+                                 :reason reason)
+                new-user (first (insert! (:db system) "ssm.users" user-map))]
+            (if new-user
+              (resp/ok (select-keys new-user [:id :email]))
+              (resp/bad-request {:message "unknown error"
+                                 :data new-user})))
+
+          (catch PSQLException e
+            (check-conflict e))
+          (catch Exception e
+            (println "Exception" e)
+            (.printStackTrace e)
+            (resp/internal-server-error {:message (.getMessage e)})))))))
 
 (defn login
   [email password]
