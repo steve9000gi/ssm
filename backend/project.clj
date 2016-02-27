36dfe28be366897c4188622878ca5bdfff2ec0df
Jeff Terrell <jeff@altometrics.com>
2016-02-27 12:31:51 -0500

    Bump dependency versions

--- backend/project.clj
+++ backend/project.clj
@@ -1,18 +1,18 @@
 (defproject backend "0.5.1"
   :description "Backend persistence layer for SSM tool"
   :url "http://syssci.renci.org/ssm/"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.stuartsierra/component "0.2.3"]
-                 [reloaded.repl "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [reloaded.repl "0.2.1"]
                  [http-kit "2.1.19"]
                  [compojure "1.4.0"]
-                 [metosin/ring-http-response "0.6.3"]
-                 [ring-middleware-format "0.5.0"]
+                 [metosin/ring-http-response "0.6.5"]
+                 [ring-middleware-format "0.7.0"]
                  [ring-cors "0.1.7"]
                  [com.cemerick/friend "0.2.1"
                   :exclusions [org.clojure/core.cache]]
-                 [org.postgresql/postgresql "9.4-1201-jdbc4"]
-                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4.1208"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [com.jolbox/bonecp "0.8.0.RELEASE"]
                  [cheshire "5.5.0"]
                  [ragtime "0.5.2"]]
