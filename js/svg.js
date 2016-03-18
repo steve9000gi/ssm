0786a0ce0d8c00378810292bf39fa547e2eb99bb
Jeff Terrell <jeff@altometrics.com>
2016-03-17 22:52:37 -0400

    Expose SVG width and height
    We'll need it for the wizard's force layout.

--- js/svg.js
+++ js/svg.js
@@ -4,19 +4,21 @@ exports.nodes = [];
 exports.links = [];
 exports.shapeGroups = null;
 exports.edgeGroups = null;
+exports.width = null;
+exports.height = null;
 
 exports.setup = function(d3) {
-  var wizardEl = document.getElementById('wizard'),
-      width = window.innerWidth,
-      height = window.innerHeight - wizardEl.clientHeight;
+  var wizardEl = document.getElementById('wizard');
+  exports.width = window.innerWidth;
+  exports.height = window.innerHeight - wizardEl.clientHeight;
   // MAIN SVG:
   d3.select("#topGraphDiv").append("div")
     .attr("id", "mainSVGDiv");
   exports.svg = d3.select("#mainSVGDiv").append("svg")
     .attr("id", "mainSVG")
     .style("font-family", "arial")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", exports.width)
+    .attr("height", exports.height);
   // The group that contains the main SVG element:
   exports.svgG = exports.svg.append("g")
     .classed("graph", true)
