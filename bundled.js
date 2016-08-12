bfd17e06fe59552e258460a3f91e09ce2ec266a5
Steve Chall <stevec@renci.org>
2016-08-12 14:43:24 -0400

    new yaml files; new dropdown lists

--- bundled.js
+++ bundled.js
@@ -401,12 +401,106 @@ var animals = [
   'zebra'
 ];
 
+var rolesCYSHCN = [
+  'Parent',
+  'Guardian'
+]; 
+
+var responsibilitiesCYSHCN = [
+  'Coordinate and Advocate for my child’s specific needs',
+  'Provide basic daily care (daily supports, bathing, feeding, exercise, etc.)',
+  'Provide financial support/stability',
+  'Healthcare',
+  'Plan for their future and transitions',
+  'Provide emotional support, connect my child to family members and the community',
+  'Self-care',
+  'Coordinate mental health supports',
+  'Education',
+  'Transport'
+];
+
+var needsCYSHCN = [
+  'Money/financial assistance',
+  'Information, Knowledge, or the skills to find information',
+  'Insurance benefits that cover necessary services',
+  'Healthcare (providers, equipment, etc.)',
+  'Equipment/supplies for basic home care',
+  'Flexible work schedule',
+  'Transportation',
+  'Respite care',
+  'Communication with others',
+  'Patient Care Assistant (PCA)',
+  'Supervision',
+  'Understanding (from community, friends, etc.)',
+  'Supports for school',
+  'Opportunities for community involvement',
+  'Support for other family/caregiver needs (emotional support, patience, positive attitude, etc.)',
+  'Time (time management, more time to complete everything etc.)'
+];
+
+var resourcesCYSHCN = [
+  'Special Education and school supports',
+  'Health Providers (clinics, health professionals, at-home care, etc.)',
+  'Agencies (Social Services, Department of Health, etc.)',
+  'Network of family and friends',
+  'Family advocacy groups',
+  'Health insurance/waivers',
+  'Personal philosophies, practices, etc.',
+  'County resources',
+  'Personal expenses for transportation (personal car, taxis, etc.)',
+  'Internet-based help',
+  'Flexible job that allows fluid hours',
+  'Coordination services or case managers',
+  'Support for supervision and socialization',
+  'Supplies for basic day-to-day needs'
+];
+
+var wishesCYSHCN = [
+  'Better special needs services in school',
+  'Care coordination',
+  'Child’s happiness and safety',
+  'Child’s independence and success',
+  'Money/financial support',
+  'Opportunities and equipment for community involvement and activeness',
+  'Transportation',
+  'Information/knowledge/training',
+  'Services',
+  'Policies and legislation',
+  'Medical/treatment advancements',
+  'Housing solutions'
+];
+
+var responsibilitiesTitleV = [
+  'Gather evidence-informed strategies to promote breastfeeding',
+  'Increase the percent of women receiving well visits',
+  'Facilitate care coordination',
+  'Inform policy makers',
+  'Engage stakeholders and partners',
+  'Monitor MCH data',
+  'Facilitate meetings',
+  'Write reports',
+  'Oversee a team'
+];
+
+var needsTitleV = [
+  'Information',
+  'Communication skills',
+  'File sharing system',
+  'Knowledge',
+  'Facilitation skills',
+  'Real-time data',
+  'Staff',
+  'Partnerships',
+  'Supportive leadership',
+  'Funding'
+];
+
 exports.completionsByType = {
-  'role': animals,
-  'responsibility': animals,
-  'need': animals,
-  'resource': animals,
-  'wish': animals
+  'role': rolesCYSHCN,
+  'responsibility': responsibilitiesCYSHCN,
+  'need': needsCYSHCN,
+  'resource': resourcesCYSHCN,
+  'wish': wishesCYSHCN
 };
 
 },{}],6:[function(require,module,exports){
