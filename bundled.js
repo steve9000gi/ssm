3623386167bf71102479cf4763b1e0990a86441d
Steve Chall <stevec@renci.org>
2017-08-14 13:07:02 -0400

    add source code timestamp to output

--- bundled.js
+++ bundled.js
@@ -2871,7 +2871,8 @@ exports.getMapObject = function(d3) {
     "county": modWizard.county,
     "city": modWizard.city,
     "reason": modWizard.reason,
-    "version": "ssm-wizard-TitleX"
+    "version": "ssm-wizard-TitleX",
+    "timestamp": "2017/08/14 12:45"
   };
   if (modWizard.wizardActive) {
     ret.wizardCurrentStep = modWizard.currentStep;
